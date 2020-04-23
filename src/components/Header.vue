488c6601f88d43e7ebfac0a81f59275de70d49e3
Rob Ranf <robranf@gmail.com>
2020-04-23 11:23:01 -0700

    updating header and search style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,33 +1,46 @@
 <template>
-<nav class="header flex items-center justify-between flex-wrap p-6 h-32">
-    <div class="flex items-center flex-shrink-0 mr-6">
-        <img src="./img/site-logo-192x192.png" alt="Site Logo" class="h-16 w-16 mr-2">
-        <form action="" class="w-full max-w-small">
-            <div class="flex items-center border-b border-b-2 border-white-500 py-2">
-                <input type="text" class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none">
-            </div>
-        </form>
+  <nav class="header flex mb-4 items-center h-32">
+    <div class="flex items-center w-1/3">
+      <img
+        src="./img/site-logo-192x192.png"
+        alt="Site Logo"
+        class="h-16 w-16 ml-4 mr-2"
+      />
+      <span class="header__title text-3xl font-bold">WaifuBook</span>
     </div>
-    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-        <div class="header__links text-xl lg:flex-grow">
-            <a href="/" class="block mt-4 lg:inline-block lg:mt-0 hover:shadow-inner mr-4">
-            Home
-            </a>
-            <a href="/login" class="block mt-4 lg:inline-block lg:mt-0 hover:shadow-inner mr-4">
-            Login
-            </a>
+    <div class="flex w-1/3">
+      <form action="" class="w-full">
+        <div class="">
+          <input
+            type="text"
+            placeholder="Search"
+            aria-label="Search"
+            class="w-full"
+          />
         </div>
+      </form>
     </div>
-</nav>
-  
+    <div class="flex w-1/3">
+      <div class="header__links text-xl lg:flex-grow flex justify-end">
+        <a
+          href="/"
+          class="block mt-4 lg:inline-block lg:mt-0 hover:shadow-inner mr-4"
+        >
+          Home
+        </a>
+        <a
+          href="/login"
+          class="block mt-4 lg:inline-block lg:mt-0 hover:shadow-inner mr-4"
+        >
+          Login
+        </a>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
