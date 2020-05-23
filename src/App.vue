5c15579ed271e7311c9e4159395b7d242d1fd19b
Rob Ranf <robranf@gmail.com>
2020-05-23 12:36:49 -0700

    moving custom styles to app and adding outlines of mywaifu, topwaifu and footer components

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <MyWaifus />
     <WaifuDay />
     <WaifuTop />
+    <Footer />
   </div>
 </template>
 
@@ -12,6 +13,7 @@ import Header from "@/components/Header";
 import MyWaifus from "@/components/MyWaifus.vue";
 import WaifuDay from "@/components/WaifuDay.vue";
 import WaifuTop from "@/components/WaifuTop.vue";
+import Footer from "@/components/Footer.vue";
 
 export default {
   name: "App",
@@ -19,18 +21,33 @@ export default {
     Header,
     MyWaifus,
     WaifuDay,
-    WaifuTop
+    WaifuTop,
+    Footer
   }
 };
 </script>
 
 <style lang="scss">
+/* color palette:
+main orange: ff971d,
+light orange: ffe8d6,
+orange off white: f9f6f7,
+white: ffffff */
+
 #app {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin: 2rem auto;
+}
+
+/* custom global styles */
+.-bg-color-main {
+  background-color: #ff971d;
+}
+.-bg-color-off-orange {
+  background-color: #f9f6f7;
 }
 
 @media (max-width: 700px) {
---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="footer flex mb-4 h-32 -bg-color-main">
+    <div class="flex w-1/3"></div>
+    <div class="flex w-1/3"></div>
+    <div class="flex w-1/3">
+      <span class="text-white text-sm">
+        © 2020 Emiya Consulting LLC
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
+
+<style lang="scss" scoped></style>
