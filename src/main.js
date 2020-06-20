41b50f656c8ea4a0ea528ea5d0a726680b1d589b
Rob Ranf <robranf@gmail.com>
2020-06-20 13:28:52 -0700

    adding component auto registration, base component registration, and starting data

--- src/main.js
+++ src/main.js
@@ -1,9 +1,38 @@
 import Vue from "vue";
 import App from "./App.vue";
+import router from "router.js";
+import store from "store.js";
+import upperFirst from "lodash/upperFirst";
+import camelCase from "lodash/camelCase";
+import Vuelidate from "vuelidate";
+
 import "./assets/main.css";
+
+Vue.use(Vuelidate);
+
+const requireComponent = require.context(
+  "./components",
+  false,
+  /Base[A-Z]\w+\.(vue|js)$/
+);
+
+requireComponent.keys().forEach(function(fileName) {
+  const componentConfig = requireComponent(fileName);
+  const componentName = upperFirst(
+    camelCase(
+      fileName
+        .split("/")
+        .pop()
+        .replace(/\.\w+$/, "")
+    )
+  );
+  Vue.component(componentName, componentConfig.default || componentConfig);
+});
 
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
+  store,
   render: (h) => h(App)
 }).$mount("#app");
