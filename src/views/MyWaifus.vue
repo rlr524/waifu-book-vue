aee8b98f7d35dafe9a815c49bf21e27d32c2fbf3
Rob Ranf <robranf@gmail.com>
2020-12-11 12:45:14 -0800

    adding jsconfig for vetur and updating some styles on mywaifu

--- src/views/MyWaifus.vue
+++ src/views/MyWaifus.vue
@@ -1,19 +1,20 @@
 <template>
   <div class="my-waifu pl-2 md:pl-5 lg:pl-0">
     <ul
-      class="flex justify-between pt-4 pb-4 scrollbar overflor-x-auto xxl-no-scrollbar"
+      class="flex justify-between pt-4 pb-4 scrollbar overflow-x-auto xxl-no-scrollbar"
     >
       <li>
         <a
           href="#"
-          class="mr-4 py-6 h-full w-16 flex flex-col justify-between items-center rounded hover:shadow-dark hover-transition -bg-color-main text-white hover:-bg-color-main-darker"
+          class="mr-4 py-6 h-full w-16 flex flex-col justify-between items-center rounded hover:shadow-dark hover-transition bg-blue text-white hover:bg-orange-darker"
+          aria-label="View my waifus"
         >
-          <div flex>
-            <span class="font-light">
+          <div flex flex-col items-center>
+            <h3 class="font-light">
               My Waifus
-            </span>
+            </h3>
           </div>
-        </a>
+          </a>
       </li>
       <li
         class="flex flex-col font-body relative rounded mr-5 text-xs"
